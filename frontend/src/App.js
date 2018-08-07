d5483bc1b423105a39a6ee17c641c2c004a6164c
rishidk22 <rishidk22@gmail.com>
2018-08-06 21:00:40 -0500

    changed from axios to xhp and fixed chatbot display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,105 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
+import PropTypes from 'prop-types'
 import ChatBot from 'react-simple-chatbot';
+import styledComponents from "styled-components"
 
-function handleRequest(chat_input){
+class AnalysisBot extends Component {
+  constructor(props) {
+    super(props);
 
-    axios.post("https://secret-chamber-49228.herokuapp.com/process_chat", {'message': chat_input})
-        .then(res => {
-            console.log(res.data)
-            return String(res.data)
-        })
+    this.state = {
+      loading: true,
+      result: '',
+      trigger: false,
+    };
 
+    this.triggetNext = this.triggetNext.bind(this);
+  }
+
+  componentWillMount() {
+    const self = this;
+    const { steps } = this.props;
+    const search = steps.chatInput.value;
+
+
+    const queryUrl = `https://secret-chamber-49228.herokuapp.com/process_chat`;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', readyStateChange);
+
+    function readyStateChange() {
+      if (this.readyState === 4) {
+        const data = this.responseText;
+        console.log(data)
+
+        self.setState({ loading: false, result: data });
+
+      }
+    }
+
+    var params = {
+      'message': search
+    }
+    xhr.open('POST', queryUrl);
+    xhr.send(JSON.stringify(params));
+  }
+
+  triggetNext() {
+    this.setState({ trigger: true }, () => {
+      this.props.triggerNextStep();
+    });
+  }
+
+  render() {
+    const { trigger, loading, result } = this.state;
+
+    return (
+      <div className="dbpedia">
+        {result }
+      </div>
+
+    );
+  }
 }
 
-const steps = [{
-                    id: '1',
-                        message: 'Hello World',
-                    trigger: '2',
-                },
-                {
-                    id: '2',
-                    user: true,
-                    trigger: '3',
-                },
-                {
-                    id: '3',
-                    message: ({ previousValue, steps }) => handleRequest(previousValue),
-                    trigger: '2'
-                }]
+AnalysisBot.propTypes = {
+  steps: PropTypes.object,
+  triggerNextStep: PropTypes.func,
+};
+
+AnalysisBot.defaultProps = {
+  steps: undefined,
+  triggerNextStep: undefined,
+};
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ChatBot steps={steps} hideHeader={true} width={100}/>
+        <ChatBot steps={
+                [{
+                        id: '1',
+                        message: 'Hello World',
+                        trigger: 'chatInput',
+                    },
+                    {
+                        id: 'chatInput',
+                        user: true,
+                        trigger: '3',
+                    },
+                    {
+                        id: '3',
+                        component: <AnalysisBot/>,
+                        asMessage: true,
+                        trigger: 'chatInput'
+                    }
+
+                ]}
+                 hideHeader={true}
+                 width={100}/>
       </div>
     );
   }
